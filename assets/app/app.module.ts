4df9a7da7ddca5561d5551ec4fa2f9c935417146
Levi <leviemmanuelfrias@gmail.com>
2018-03-20 00:56:43 +0800

    fix charts

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,12 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from "./app.component";
+import { AppBodyContentComponent } from './appbodycontent/appbodycontent.component';
+import { AppOutline } from './appoutline/appoutline.component';
+import { ChartsModule } from 'ng2-charts';
+import { LineChartDemoComponent } from './linechart/linechart.component';
+import { BarChartDemoComponent } from './barchart/barchart.component';
+import { DoughnutChartDemoComponent } from './doughnutchart/doughnut.component';
+import { BaseChartDemoComponent } from './dynamicchart/dynamicchart.component';
+import { PieChartDemoComponent } from './piechart/piechart.component';
+import { PolarAreaChartDemoComponent } from './polarchart/polar.component';
+import { RadarChartDemoComponent } from './radarchart/radar.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        AppBodyContentComponent,
+        AppOutline,
+        LineChartDemoComponent,
+        BarChartDemoComponent,
+        DoughnutChartDemoComponent,
+        BaseChartDemoComponent,
+        PieChartDemoComponent,
+        PolarAreaChartDemoComponent,
+        RadarChartDemoComponent
     ],
-    imports: [BrowserModule],
+    imports: [BrowserModule, ChartsModule],
     bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ assets/app/appbodycontent/appbodycontent.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-body',
+    templateUrl: './appbodycontent.component.html'
+})
+
+export class AppBodyContentComponent{
+    
+};
