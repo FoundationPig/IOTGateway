899ed416c0c94b0181faf1254a8b4990ec283d47
Ghino Punzalan <mistelteyn@gmail.com>
2018-03-27 03:11:53 +0800

    Added Routing

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from "./app.component";
 import { AppBodyContentComponent } from './appbodycontent/appbodycontent.component';
 import { AppOutline } from './appoutline/appoutline.component';
+import { RouterModule, Routes } from '@angular/router'
 import { ChartsModule } from 'ng2-charts';
 import { LineChartDemoComponent } from './linechart/linechart.component';
 import { BarChartDemoComponent } from './barchart/barchart.component';
@@ -13,6 +13,15 @@ import { PieChartDemoComponent } from './piechart/piechart.component';
 import { PolarAreaChartDemoComponent } from './polarchart/polar.component';
 import { RadarChartDemoComponent } from './radarchart/radar.component';
 
+const appRoutes: Routes = [
+    {path:'basechart', component:BaseChartDemoComponent},
+    {path:'piechart', component:PieChartDemoComponent},
+    {path:'linechart', component:LineChartDemoComponent},
+    {path:'barchart', component:BarChartDemoComponent},
+    {path:'radarchart', component:RadarChartDemoComponent},
+    {path:'doughnutchart', component:DoughnutChartDemoComponent},
+    {path:'polarareachart', component:PolarAreaChartDemoComponent}
+]
 @NgModule({
     declarations: [
         AppComponent,
@@ -26,7 +35,11 @@ import { RadarChartDemoComponent } from './radarchart/radar.component';
         PolarAreaChartDemoComponent,
         RadarChartDemoComponent
     ],
-    imports: [BrowserModule, ChartsModule],
+    imports: [
+        BrowserModule, 
+        ChartsModule,
+        RouterModule.forRoot(appRoutes)
+        ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
